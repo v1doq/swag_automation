abe50be325289c7ef8b523267a3a7424ab05bee4
nGervasyuk <satyricon10186124>
2018-02-28 14:40:26 +0200

    add auth component and step

---
+++ src/main/java/projects/com/password/tools/steps/LoginStep.java
@@ -0,0 +1,38 @@
+package projects.com.password.tools.steps;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import projects.com.password.tools.components.LoginComponent;
+
+import static common.DefaultConstant.PASSWORD_PASS_TOOLS;
+import static common.DefaultConstant.USERNAME_PASS_TOOLS;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+import static settings.TestConfig.getProperty;
+
+public class LoginStep {
+
+    private LoginComponent component;
+
+    public LoginStep(WebDriver driver) {
+        this.component = new LoginComponent(driver);
+    }
+
+    @Step("Authorize by existing user")
+    public void authorization(){
+        openLandingPage();
+        login(USERNAME_PASS_TOOLS, PASSWORD_PASS_TOOLS);
+    }
+
+    @Step("Open landing page")
+    public void openLandingPage() {
+        component.open(getProperty("password.tools.url"));
+    }
+
+    @Step("Login with user credential")
+    public void login(String username, String pass) {
+        component.getUsernameInput().sendKeys(username);
+        component.getPasswordInput().sendKeys(pass);
+        component.getSubmitButton().click();
+        component.assertThat(visibilityOf(component.getLogoutButton()));
+    }
+}
